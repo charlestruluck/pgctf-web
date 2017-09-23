a4c346a7983435293ebf75752bb206329e80c291
charlestruluck <github@ctruluck.com>
2017-09-23 12:48:43 -0400

    I think I fixed it now...

--- index.js
+++ index.js
@@ -27,3 +27,6 @@ app.use("*",function(req,res){
 	  res.sendFile(path + "404.html");
 });
 
+app.listen(app.get('port'), function() {
+	  console.log('Node app is running on port', app.get('port'));
+});
